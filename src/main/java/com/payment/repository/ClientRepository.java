ba3ba196e17fccd7aaf43c59f78cec2754b924ab
George Niculae <george-cosmin.niculae@endava.com>
2022-06-24 12:34:18 +0300

    Updating repositories

--- src/main/java/com/payment/repository/ClientRepository.java
+++ src/main/java/com/payment/repository/ClientRepository.java
@@ -4,7 +4,18 @@ import com.payment.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
+    List<Client> findByFirstName(String firstName);
+
+
+    List<Client> findByLastName(String lastName);
+
+    Optional<Client> findByPhoneNumber(String phoneNumber);
+
+    void deleteByPhoneNumber(String phoneNumber);
 }
--- src/main/java/com/payment/repository/PaymentRepository.java
+++ src/main/java/com/payment/repository/PaymentRepository.java
@@ -4,7 +4,17 @@ import com.payment.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
+    Optional<Payment> findByClientId(Long id);
+
+    List<Payment> findByClientFirstName(String firstName);
+
+    List<Payment> findByClientLastName(String lastName);
+
+    void deleteByClientId(Long id);
 }
