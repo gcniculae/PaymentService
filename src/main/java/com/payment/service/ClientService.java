0799d2f2fb9186ec91db7e0092dd55e1cf5f85fe
George Niculae <george-cosmin.niculae@endava.com>
2022-06-24 12:35:18 +0300

    Updating services

--- src/main/java/com/payment/service/ClientService.java
+++ src/main/java/com/payment/service/ClientService.java
@@ -1,8 +1,14 @@
 package com.payment.service;
 
+import com.payment.entity.Client;
+import com.payment.exception.NotFoundException;
 import com.payment.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClientService {
@@ -12,5 +18,61 @@ public class ClientService {
     @Autowired
     public ClientService(ClientRepository clientRepository) {
         this.clientRepository = clientRepository;
+    }
+
+    public Client saveClient(Client client) {
+        return clientRepository.save(client);
+    }
+
+    public List<Client> findAllClients() {
+        return clientRepository.findAll();
+    }
+
+    public Client findClientById(Long id) {
+        Optional<Client> clientOptional = clientRepository.findById(id);
+
+        if (clientOptional.isPresent()) {
+            return clientOptional.get();
+        } else {
+            throw new NotFoundException("No such client found.", "client.not.found");
+        }
+    }
+
+    public List<Client> findClientsByFirstName(String firstName) {
+        return clientRepository.findByFirstName(firstName);
+    }
+
+    public List<Client> findClientsByLastName(String lastName) {
+        return clientRepository.findByLastName(lastName);
+    }
+
+    public Client findClientByPhoneNumber(String phoneNumber) {
+        Optional<Client> optionalClient = clientRepository.findByPhoneNumber(phoneNumber);
+
+        if (optionalClient.isPresent()) {
+            return optionalClient.get();
+        } else {
+            throw new NotFoundException("No such client found.", "client.not.found");
+        }
+    }
+
+    public Client updateClient(Long id, Client client) {
+        Client clientById = findClientById(id);
+        client.setId(clientById.getId());
+
+        return saveClient(client);
+    }
+
+    public void deleteClientById(Long id) {
+        Client clientById = findClientById(id);
+
+        clientRepository.deleteById(clientById.getId());
+    }
+
+    @Transactional
+    public void deleteClientByPhoneNumber(String phoneNumber) {
+        Client clientByPhoneNumber = findClientByPhoneNumber(phoneNumber);
+
+        clientRepository.deleteByPhoneNumber(clientByPhoneNumber.getPhoneNumber());
     }
 }
--- src/main/java/com/payment/service/PaymentService.java
+++ src/main/java/com/payment/service/PaymentService.java
@@ -1,8 +1,13 @@
 package com.payment.service;
 
+import com.payment.entity.Payment;
+import com.payment.exception.NotFoundException;
 import com.payment.repository.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PaymentService {
@@ -12,5 +17,60 @@ public class PaymentService {
     @Autowired
     public PaymentService(PaymentRepository paymentRepository) {
         this.paymentRepository = paymentRepository;
+    }
+
+    public Payment savePayment(Payment payment) {
+        return paymentRepository.save(payment);
+    }
+
+    public List<Payment> findAllPayments() {
+        return paymentRepository.findAll();
+    }
+
+    public Payment findPaymentById(Long id) {
+        Optional<Payment> optionalPayment = paymentRepository.findById(id);
+
+        if (optionalPayment.isPresent()) {
+            return optionalPayment.get();
+        } else {
+            throw new NotFoundException("No such payment found.", "payment.not.found");
+        }
+    }
+
+    public Payment findPaymentByClientId(Long id) {
+        Optional<Payment> optionalPayment = paymentRepository.findByClientId(id);
+
+        if (optionalPayment.isPresent()) {
+            return optionalPayment.get();
+        } else {
+            throw new NotFoundException("No such payment found.", "payment.not.found");
+        }
+    }
+
+    public List<Payment> findPaymentsByClientFirstName(String firstName) {
+        return paymentRepository.findByClientFirstName(firstName);
+    }
+
+    public List<Payment> findPaymentsByClientLastName(String lastName) {
+        return paymentRepository.findByClientLastName(lastName);
+    }
+
+    public Payment updatePayment(Long id, Payment payment) {
+        Payment paymentById = findPaymentById(id);
+        payment.setId(paymentById.getId());
+
+        return savePayment(payment);
+    }
+
+    public void deletePaymentById(Long id) {
+        Payment paymentById = findPaymentById(id);
+
+        paymentRepository.deleteById(paymentById.getId());
+    }
+
+    public void deletePaymentByClientId(Long id) {
+        Payment paymentByClientId = findPaymentByClientId(id);
+
+        paymentRepository.deleteByClientId(paymentByClientId.getClient().getId());
     }
 }
