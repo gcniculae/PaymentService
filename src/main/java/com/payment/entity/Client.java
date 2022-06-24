45cc3e53807f4db118ba5fc0c77e759354ecdac9
George Niculae <george-cosmin.niculae@endava.com>
2022-06-24 10:44:45 +0300

    Updating entities

--- src/main/java/com/payment/entity/Client.java
+++ src/main/java/com/payment/entity/Client.java
@@ -1,13 +1,22 @@
 package com.payment.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import java.util.List;
 
 @Entity
+@NoArgsConstructor
+@Getter
+@Setter
 public class Client extends BaseEntity {
+
+    private String firstName;
+    private String lastName;
 
     @OneToMany(mappedBy = "client")
     private List<Payment> payments;
-    private double amount;
 }
--- src/main/java/com/payment/entity/Payment.java
+++ src/main/java/com/payment/entity/Payment.java
@@ -1,10 +1,19 @@
 package com.payment.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 
 @Entity
+@NoArgsConstructor
+@Getter
+@Setter
 public class Payment extends BaseEntity {
+
+    private double amount;
 
     @ManyToOne
     private Client client;
