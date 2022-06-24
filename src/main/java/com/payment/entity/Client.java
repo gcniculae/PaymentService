0406f90f12bb737a27386ac25d36e2a0a4078ce0
George Niculae <george-cosmin.niculae@endava.com>
2022-06-24 10:29:37 +0300

    Updating entities

--- src/main/java/com/payment/entity/Client.java
+++ src/main/java/com/payment/entity/Client.java
@@ -1,9 +1,13 @@
 package com.payment.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @Entity
 public class Client extends BaseEntity {
 
-
+    @OneToMany(mappedBy = "client")
+    private List<Payment> payments;
+    private double amount;
 }
--- src/main/java/com/payment/entity/Payment.java
+++ src/main/java/com/payment/entity/Payment.java
@@ -1,9 +1,11 @@
 package com.payment.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Payment extends BaseEntity {
 
-
+    @ManyToOne
+    private Client client;
 }
