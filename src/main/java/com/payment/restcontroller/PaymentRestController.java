e07246a533a93f1536cac531e1a864e2b8274b15
George Niculae <george-cosmin.niculae@endava.com>
2022-06-24 15:29:42 +0300

    Updating rest controllers

--- src/main/java/com/payment/restcontroller/PaymentRestController.java
+++ src/main/java/com/payment/restcontroller/PaymentRestController.java
@@ -1,11 +1,20 @@
 package com.payment.restcontroller;
 
 import com.payment.converter.PaymentConverter;
+import com.payment.dto.PaymentDto;
+import com.payment.entity.Payment;
 import com.payment.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@RequestMapping(path = "/payments")
+@CrossOrigin(origins = "*")
 public class PaymentRestController {
 
     private final PaymentService paymentService;
@@ -15,5 +24,62 @@ public class PaymentRestController {
     public PaymentRestController(PaymentService paymentService, PaymentConverter paymentConverter) {
         this.paymentService = paymentService;
         this.paymentConverter = paymentConverter;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PaymentDto>> findPayments(@RequestParam(name = "all", required = false, defaultValue = "false") Boolean all,
+                                                         @RequestParam(name = "ids", required = false) List<Long> ids,
+                                                         @RequestParam(name = "firstName", required = false) String firstName,
+                                                         @RequestParam(name = "lastName", required = false) String lastName) {
+        List<Payment> payments = new ArrayList<>();
+
+        if (all) {
+            payments = paymentService.findAllPayments();
+        } else if (ids != null) {
+            payments = paymentService.findPaymentsById(ids);
+        } else if (firstName != null) {
+            payments = paymentService.findPaymentsByClientFirstName(firstName);
+        } else if (lastName != null) {
+            payments = paymentService.findPaymentsByClientLastName(lastName);
+        }
+
+        return ResponseEntity.ok(paymentConverter.convertEntityListToDtoList(payments));
+    }
+
+    @GetMapping(path = "/single")
+    public ResponseEntity<PaymentDto> findPayment(@RequestParam(name = "id", required = false) Long id,
+                                                  @RequestParam(name = "clientId", required = false) Long clientId) {
+        Payment payment = new Payment();
+
+        if (id != null) {
+            payment = paymentService.findPaymentById(id);
+        } else if (clientId != null) {
+            payment = paymentService.findPaymentByClientId(id);
+        }
+
+        return ResponseEntity.ok(paymentConverter.convertEntityToDto(payment));
+    }
+
+    @PostMapping
+    public ResponseEntity<PaymentDto> addClient(@Valid @RequestBody PaymentDto paymentDto) {
+        Payment payment = paymentConverter.convertDtoToEntity(paymentDto);
+        Payment savedPayment = paymentService.savePayment(payment, paymentDto.getClientId());
+
+        return ResponseEntity.ok(paymentConverter.convertEntityToDto(savedPayment));
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<PaymentDto> updatePayment(@PathVariable("id") Long id, @Valid @RequestBody PaymentDto paymentDto) {
+        Payment payment = paymentConverter.convertDtoToEntity(paymentDto);
+        Payment updatedPayment = paymentService.updatePayment(id, payment, paymentDto.getClientId());
+
+        return ResponseEntity.ok(paymentConverter.convertEntityToDto(updatedPayment));
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<PaymentDto> deletePayment(@PathVariable("id") Long id) {
+        paymentService.deletePaymentById(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
