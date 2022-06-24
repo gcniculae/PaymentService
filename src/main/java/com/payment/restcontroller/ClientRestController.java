10fc0c5d0528ff4335e64403494f1d68a5632a14
George Niculae <george-cosmin.niculae@endava.com>
2022-06-24 10:45:02 +0300

    Creating rest controllers

---
+++ src/main/java/com/payment/restcontroller/ClientRestController.java
@@ -0,0 +1,9 @@
+package com.payment.restcontroller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClientRestController {
+
+
+}
---
+++ src/main/java/com/payment/restcontroller/PaymentRestController.java
@@ -0,0 +1,8 @@
+package com.payment.restcontroller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentRestController {
+    
+}
