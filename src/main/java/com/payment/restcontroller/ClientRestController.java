923a874f92b9f36c262386fa286c0cdbdbe872dd
George Niculae <george-cosmin.niculae@endava.com>
2022-06-24 11:22:28 +0300

    Updating rest controllers

--- src/main/java/com/payment/restcontroller/ClientRestController.java
+++ src/main/java/com/payment/restcontroller/ClientRestController.java
@@ -1,9 +1,19 @@
 package com.payment.restcontroller;
 
+import com.payment.converter.ClientConverter;
+import com.payment.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClientRestController {
 
+    private final ClientService clientService;
+    private final ClientConverter clientConverter;
 
+    @Autowired
+    public ClientRestController(ClientService clientService, ClientConverter clientConverter) {
+        this.clientService = clientService;
+        this.clientConverter = clientConverter;
+    }
 }
--- src/main/java/com/payment/restcontroller/PaymentRestController.java
+++ src/main/java/com/payment/restcontroller/PaymentRestController.java
@@ -1,8 +1,19 @@
 package com.payment.restcontroller;
 
+import com.payment.converter.PaymentConverter;
+import com.payment.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PaymentRestController {
-    
+
+    private final PaymentService paymentService;
+    private final PaymentConverter paymentConverter;
+
+    @Autowired
+    public PaymentRestController(PaymentService paymentService, PaymentConverter paymentConverter) {
+        this.paymentService = paymentService;
+        this.paymentConverter = paymentConverter;
+    }
 }
