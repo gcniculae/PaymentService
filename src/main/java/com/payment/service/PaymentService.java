1c560f89565939ef351f2ffeffc03b7050f068a7
George Niculae <george-cosmin.niculae@endava.com>
2022-06-24 15:29:27 +0300

    Updating services

--- src/main/java/com/payment/service/PaymentService.java
+++ src/main/java/com/payment/service/PaymentService.java
@@ -1,30 +1,52 @@
 package com.payment.service;
 
+import com.payment.dto.PaymentDto;
 import com.payment.entity.Payment;
 import com.payment.exception.NotFoundException;
 import com.payment.repository.PaymentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class PaymentService {
 
     private final PaymentRepository paymentRepository;
+    private final ClientService clientService;
 
     @Autowired
-    public PaymentService(PaymentRepository paymentRepository) {
+    public PaymentService(PaymentRepository paymentRepository, ClientService clientService) {
         this.paymentRepository = paymentRepository;
+        this.clientService = clientService;
     }
 
-    public Payment savePayment(Payment payment) {
+    public Payment savePayment(Payment payment, Long clientId) {
+        payment.setClient(clientService.findClientById(clientId));
+
         return paymentRepository.save(payment);
     }
 
     public List<Payment> findAllPayments() {
         return paymentRepository.findAll();
+    }
+
+    public List<Payment> findPaymentsById(List<Long> ids) {
+        return ids.stream()
+                .map(this::findPaymentById)
+                .collect(Collectors.toList());
+    }
+
+    public List<Payment> findPaymentsByClientFirstName(String firstName) {
+        return paymentRepository.findByClientFirstName(firstName);
+    }
+
+    public List<Payment> findPaymentsByClientLastName(String lastName) {
+        return paymentRepository.findByClientLastName(lastName);
     }
 
     public Payment findPaymentById(Long id) {
@@ -47,30 +69,16 @@ public class PaymentService {
         }
     }
 
-    public List<Payment> findPaymentsByClientFirstName(String firstName) {
-        return paymentRepository.findByClientFirstName(firstName);
-    }
-
-    public List<Payment> findPaymentsByClientLastName(String lastName) {
-        return paymentRepository.findByClientLastName(lastName);
-    }
-
-    public Payment updatePayment(Long id, Payment payment) {
+    public Payment updatePayment(Long id, Payment payment, Long clientId) {
         Payment paymentById = findPaymentById(id);
         payment.setId(paymentById.getId());
 
-        return savePayment(payment);
+        return savePayment(payment, clientId);
     }
 
     public void deletePaymentById(Long id) {
         Payment paymentById = findPaymentById(id);
 
         paymentRepository.deleteById(paymentById.getId());
-    }
-
-    public void deletePaymentByClientId(Long id) {
-        Payment paymentByClientId = findPaymentByClientId(id);
-
-        paymentRepository.deleteByClientId(paymentByClientId.getClient().getId());
     }
 }
